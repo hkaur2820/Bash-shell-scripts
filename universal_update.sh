71e1754998829689d72ce3e6b3e881d5b55123f9
hkaur2820 <harmank2401@gmail.com>
2024-01-18 18:17:37 +0530

    Added universal_update.sh file to the repo

---
+++ universal_update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+package=/etc/os-release
+
+if grep -q "Ubuntu" $package
+then
+  sudo apt-get update
+  sudo apt dist-upgrade
+fi
+if grep -q "Arch" $package
+then
+  sudo pacman -Syu
+fi
+if grep -q "CentOS" $package
+then
+  sudo yum upgrade
+fi
+if grep -q "Red Hat" $package
+then
+  sudo dnf upgrade
+fi
