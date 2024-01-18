a73e0055fcfe639ff7df6131af0dca5577e4e77a
hkaur2820 <harmank2401@gmail.com>
2024-01-18 16:03:25 +0530

    Used command to check existance by removing f

--- install_command.sh
+++ install_command.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #Author: Harman Kaur
-#Installing htop here as an example.
-
-path=/usr/bin/htop
-
-if [ -f $path ]
+#Using htop as an example
+path=htop
+#Checking the existance of a command.
+if command -v $path
   then
     echo "$path is available"
     $path
   else
     echo "$path is not availble.Installing it right now!..."
-    sudo apt update && sudo apt install -y htop
+    sudo apt update && sudo apt install -y $path
 fi
